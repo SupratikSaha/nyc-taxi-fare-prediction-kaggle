a80e905d5ab171674d248b63cf0be6dff369aa26
totoslg <totoslg@gmail.com>
2020-06-30 20:18:05 -0700

    Add further training data processing and format code

--- src/filter_train_set.py
+++ src/filter_train_set.py
@@ -6,18 +6,18 @@ import pandas as pd
 import numpy as np
 
 ###############################################################################
-## load training set
+# load training set
 ###############################################################################
 data = pd.read_csv('../data/train.csv')
 
 ###############################################################################
-## filter outlier
+# filter outlier
 ###############################################################################
 # remove the fare smaller than $2.5
-data = data[data['fare_amount'] >= 2.5] 
+data = data[data['fare_amount'] >= 2.5]
 
 # remove the fare larger than $500
-data = data[data['fare_amount'] <= 500] 
+data = data[data['fare_amount'] <= 500]
 
 # remove longitudes outliers
 data = data[data['pickup_longitude'] >= -80]
@@ -25,24 +25,24 @@ data = data[data['pickup_longitude'] <= -70]
 data = data[data['dropoff_longitude'] >= -80]
 data = data[data['dropoff_longitude'] <= -70]
 
-# remove latittudes outliers
+# remove latitudes outliers
 data = data[data['pickup_latitude'] >= 35]
-data = data[data['pickup_latitude'] <= 45] 
+data = data[data['pickup_latitude'] <= 45]
 data = data[data['dropoff_latitude'] >= 35]
 data = data[data['dropoff_latitude'] <= 45]
 
 # remove 0 or larger than 10 passenger
-data = data[data['passenger_count'] > 0] 
+data = data[data['passenger_count'] > 0]
 data = data[data['passenger_count'] < 10]
 
 # remove missing samples
-data = data.dropna() # (54077798, 8)
+data = data.dropna()  # (54077798, 8)
 
 # split key into datetime and ID
 data['ID'] = data['key'].str.split('.').str[-1].astype(np.int64)
 
 # remove unused feature
-data = data.drop(['key'], axis = 1)
+data = data.drop(['key'], axis=1)
 
 # save filtered training set
-data.to_csv('../filtered_data/filter_train.csv',index = False)
+data.to_csv('../filtered_data/filter_train.csv', index=False)
---
+++ src/further_filter_processed_train.py
@@ -0,0 +1,35 @@
+"""
+Filter Training Data further post processing
+"""
+
+import pandas as pd
+
+###############################################################################
+# load training set
+###############################################################################
+
+# Train holdout data
+train_file = '../data/filter_train_holdout.csv'
+
+# load data
+train_df = pd.read_csv(train_file)
+
+###############################################################################
+# filter outlier
+###############################################################################
+# Remove observations with useless values base on the test data boundary
+mask = train_df['pickup_longitude'].between(-74.3, -72.9)
+mask &= train_df['dropoff_longitude'].between(-74.3, -72.9)
+mask &= train_df['pickup_latitude'].between(40.5, 41.8)
+mask &= train_df['dropoff_latitude'].between(40.5, 41.7)
+mask &= train_df['passenger_count'].between(0, 6)  # 1 - 6
+mask &= train_df['fare_amount'].between(2, 200)  # 2.5 - 200
+
+# update training set
+train_df = train_df[mask]
+
+###############################################################################
+# save results
+###############################################################################
+# save filtered training set
+train_df.to_csv(train_file, index=False)
